ba51ca88a1e428bb41ebfcce3bcce4c9a7c5bb41
haku <ymchh1017@gmail.com>
2023-09-21 21:21:14 +0900

    helloworld

---
+++ B.Javascript(1)/01-자바스크립트-시작하기/HelloWorld.js
@@ -0,0 +1,14 @@
+console.group("MyMessage1");
+console.log("안녕하세요. Javascript1");
+console.log("안녕하세요. Javascript2");
+console.log("안녕하세요. Javascript3");
+console.groupEnd();
+
+console.group("MyMessage2");
+console.log("안녕하세요, Javascript4");
+
+console.group("MyMessage2-1");
+console.log("안녕하세요. Javascript5");
+console.log("안녕하세요. Javascript6");
+console.groupEnd;
+console.groupEnd;
