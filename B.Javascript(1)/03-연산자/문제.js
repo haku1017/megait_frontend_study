72ff235d6feb40d6efd50d74c106cc040a88ee6a
haku <ymchh1017@gmail.com>
2023-10-04 23:22:21 +0900

    업데이트

---
+++ B.Javascript(1)/03-연산자/문제.js
@@ -0,0 +1,13 @@
+// const year = 2023;
+// const age = (year - 2000) +  1;
+// console.log(`나는 ${age}세 입니다.`);
+
+//
+// const age = 24;
+// const year =  (2023 + 1) - age;
+// console.log(`나는 ${year}년도에 태어났습니다.`);
+
+//
+const numOfApples = 120;
+const basket = 123 % 10
+console.log(numOfApples % 10 > 0 || numOfApples % 10 == 0 ? 12 : 13);
